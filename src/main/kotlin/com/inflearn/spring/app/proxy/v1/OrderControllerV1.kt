7b8ea91afa7b972949c492240cabb101f3a93812
ywonp94 <ywonp94@gmail.com>
2024-12-20 12:37:15 +0900

    :construction: "Proxy V1 구현 중"

--- src/main/kotlin/com/inflearn/spring/app/proxy/v1/OrderControllerV1.kt
+++ src/main/kotlin/com/inflearn/spring/app/proxy/v1/OrderControllerV1.kt
@@ -1,10 +1,15 @@
 package com.inflearn.spring.app.proxy.v1
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.ResponseBody
 
-@RestController("/proxy")
+@Controller
+@RequestMapping("/proxy")
+@ResponseBody
+//@RestController("/proxy")
 interface OrderControllerV1 {
     @GetMapping("/v1/request")
     fun request(@RequestParam itemId: String): String
